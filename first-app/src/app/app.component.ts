748fe7158053979c9ba8f5eb1cbc633d1b13c52a
davinen-s <davinen.s@gmail.com>
2017-11-19 16:43:59 +0500

    progress chapter 3

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  firstMediaItem = {
+    id: 1,
+    name: 'Firebug',
+    medium: 'Series',
+    category: 'Science Fiction',
+    year: 2010,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  };
+
+  onMediaItemDelete(mediaItem){
+
+  }
 }
--- first-app/src/app/app.module.ts
+++ first-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MediaItemComponent } from './components/media-item/media-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MediaItemComponent
   ],
   imports: [
     BrowserModule,
